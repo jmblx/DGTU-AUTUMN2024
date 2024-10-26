b3b98a9e936986b832307cb185e3d41a80516ce4
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2024-10-27 02:25:42 +0300

    added receipts mock

--- api/src/core/di/providers/services.py
+++ api/src/core/di/providers/services.py
@@ -57,10 +57,10 @@ class ServiceProvider(Provider):
         return Firebase()
     @provide(scope=Scope.REQUEST)
     async def get_user(self, request: Request, session: AsyncSession, fb: Firebase) -> User: # noqa
-        # token_id = request.headers.get("Authorization")
-        # user_uid = (await auth_user_id_by_token(token_id)).get("id")
-        # return await session.get(User, user_uid)
-        return await session.get(User, "MPLbrcAiVPSSAdYmr1ldEmTXjJA2")
+        token_id = request.headers.get("Authorization")
+        user_uid = (await auth_user_id_by_token(token_id)).get("id")
+        return await session.get(User, user_uid)
+        # return await session.get(User, "MPLbrcAiVPSSAdYmr1ldEmTXjJA2")
 
     @provide(scope=Scope.REQUEST)
     async def get_data_by_auth_header(self, request: Request, fb: Firebase) -> UserLogin: # noqa
